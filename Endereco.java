f344104ab2d2faa309040843ab1a5ffda3c12989
brunnapontual <brunnapontual@hotmail.com>
2023-09-19 17:42:37 -0300

    Add files via upload

---
+++ Endereco.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.gov.cesarschool.poo.bonusvendas.entidade.geral;
+
+/**
+ *
+ * @author bspanudakis
+ */
+public class Endereco {
+
+    private String logradouro;
+    private int numero;
+    private String complemento;
+    private String cep;
+    private String cidade;
+    private String estado;
+    private String pais;
+
+    public Endereco(String logradouro, int numero, String complemento, String cep, String cidade, String estado, String pais) {
+        this.logradouro = logradouro;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.cep = cep;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.pais = pais;
+    }
+
+    public static String getLogradouro() {
+        return this.logradouro;
+    }
+}
